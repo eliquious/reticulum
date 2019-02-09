fafac904fd94066fadefea8d63261d57e5a2958f
Max Franks <eliquious@gmail.com>
2019-02-09 16:54:41 -0600

    Add network implementation

--- net.go
+++ net.go
@@ -14,9 +14,12 @@ const (
 
 // Network is the neural network interface.
 type Network interface {
-	Forward(vol volume.Volume, training bool)
-	Backward(index int)
-	GetCostLoss(vol volume.Volume, index int)
+	Size() int
+	Forward(vol *volume.Volume, training bool) *volume.Volume
+	Backward(index int) float64
+	GetCostLoss(vol *volume.Volume, index int) float64
+
+	// GetPrediction assumes the last layer in the network is a SoftMax layer.
 	GetPrediction() int
 	GetResponse() []layers.LayerResponse
 
@@ -77,4 +80,81 @@ func NewNetwork(defs []layers.LayerDef) (Network, error) {
 
 type network struct {
 	layers []layers.Layer
+}
+
+func (n *network) Size() int {
+	return len(n.layers)
+}
+
+func (n *network) Forward(vol *volume.Volume, training bool) *volume.Volume {
+	actions := n.layers[0].Forward(vol, training)
+	for index := 1; index < len(n.layers); index++ {
+		actions = n.layers[index].Forward(vol, training)
+	}
+	return actions
+}
+
+func (n *network) Backward(index int) float64 {
+	size := n.Size()
+
+	// Calculate loss
+	lossLayer, ok := n.layers[size-1].(layers.LossLayer)
+	if !ok {
+		panic("expecting loss layer as last layer in network")
+	}
+	loss := lossLayer.Loss(index)
+
+	// Propogate backwards
+	for index := n.Size() - 2; index >= 0; index-- {
+		n.layers[index].Backward()
+	}
+	return loss
+}
+
+func (n *network) GetCostLoss(vol *volume.Volume, index int) float64 {
+	n.Forward(vol, false)
+
+	// Calculate loss
+	lossLayer, ok := n.layers[n.Size()-1].(layers.LossLayer)
+	if !ok {
+		panic("expecting loss layer as last layer in network")
+	}
+	return lossLayer.Loss(index)
+}
+
+func (n *network) GetPrediction() int {
+	// this is a convenience function for returning the argmax
+	// prediction, assuming the last layer of the net is a softmax
+	S := n.layers[n.Size()-1]
+	if S.Type() != layers.SoftMax {
+		panic("GetPrediction assumes Softmax is the last layer in the network")
+	}
+	return layers.GetSoftMaxPrediction(S)
+}
+
+func (n *network) GetResponse() []layers.LayerResponse {
+	// accumulate parameters and gradients for the entire network
+	resp := []layers.LayerResponse{}
+	for index := 0; index < len(n.layers); index++ {
+		layerResponse := n.layers[index].GetResponse()
+		resp = append(resp, layerResponse...)
+	}
+	return resp
+}
+
+// MultiDimensionalLoss computes the total loss for each of the values given.
+func (n *network) MultiDimensionalLoss(losses []float64) float64 {
+	lossLayer, ok := n.layers[n.Size()-1].(layers.RegressionLossLayer)
+	if !ok {
+		panic("MultiDimensionalLoss assumes a Regression layer is the last layer in the network")
+	}
+	return lossLayer.MultiDimensionalLoss(losses)
+}
+
+func (n *network) DimensionalLoss(index int, value float64) float64 {
+	lossLayer, ok := n.layers[n.Size()-1].(layers.RegressionLossLayer)
+	if !ok {
+		panic("DimensionalLoss assumes a Regression layer is the last layer in the network")
+	}
+	return lossLayer.DimensionalLoss(index, value)
 }
